5eceef19053afa08372835d7058c1e37e07a0524
AhmadSaleh99 <saleehsale816@gmail.com>
2023-02-27 01:10:39 +0530

    a

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="navbar">
+      <span className="logo">Saleh Chat</span>
+      <div className="user">
+        <img
+          src="https://images.unsplash.com/photo-1507019403270-cca502add9f8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8Z2lybCUyMHByb2ZpbGV8ZW58MHx8MHx8&w=1000&q=80"
+          alt="profile"
+        />
+        <span>Saleh</span>
+        <button>Logout</button>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
