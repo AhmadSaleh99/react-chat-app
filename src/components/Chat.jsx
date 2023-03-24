6911bfd17bd0e533d5e4018f464a820bc04b884d
AhmadSaleh99 <saleehsale816@gmail.com>
2023-03-24 12:36:51 +0530

    commit

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import Cam from "../images/cam.png";
+import Add from "../images/add.png";
+import More from "../images/more.png";
 
 const Chat = () => {
-  return <div className="chat">Chat</div>;
+  return (
+    <div className="chat">
+      <div className="chatInfo">
+        <span>Jane</span>
+        <div className="chatIcons">
+          <img src={Cam} alt="Camera" />
+          <img src={Add} alt="Add" />
+          <img src={More} alt="More" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Chat;
--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 
 const Chats = () => {
-  return <div></div>;
+  return (
+    <div className="chats">
+      <div className="userChat">
+        <img
+          src="https://images.unsplash.com/photo-1507019403270-cca502add9f8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8Z2lybCUyMHByb2ZpbGV8ZW58MHx8MHx8&w=1000&q=80"
+          alt=""
+        />
+        <div className="userChatInfo">
+          <span>Jhon</span>
+          <p>Hello</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Chats;
